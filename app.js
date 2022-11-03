b0779c3f1f193f4f404260554b3a14271361d7f5
mesumalijamalvi <mesamjamalvi592@gmail.com>
2022-11-03 02:49:21 -0700

    event

---
+++ app.js
@@ -0,0 +1,6 @@
+New2=document.querySelectorAll("#btn")
+New2.forEach(function jml(Data2){
+    Data2.addEventListener("click",()=>{
+        document.write("<h1>SCIENCE AND TECHNOLOGY PARK</h1>")
+    })
+})
